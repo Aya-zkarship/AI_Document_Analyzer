5fdc00481571d99c3001ecafa12f87eb20b31198
aya.zkara@etu.uae.ac.ma <aya.zkara@etu.uae.ac.ma>
2026-05-25 13:50:25 +0100

    Deploy: Add gunicorn, whitenoise, and requirements for cloud host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-effx9_i1yw#xp-wq6gf20-a@om(p_+swu*n(f-t#9#1sd1a$gr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
